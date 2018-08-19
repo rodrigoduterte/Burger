4ba64a1e8ad6916eba6c11d2fa877466635050eb
Gabriel Ferrer <gabohermoso@gmail.com>
2018-08-18 21:31:22 -0400

    Update burger_controller.js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,24 +3,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/index",(req,res)=>{
-    // res.render("index");
     burger.all((data)=>{
         var hbs = {burgers: data};
-        console.log(hbs);
         res.render("index",hbs);
     });
 });
 router.post("/index",(req,res)=>{
-    console.log(req.body.name);
     burger.create("burger_name","devoured",req.body.name,0,(data)=>{
-        console.log("ddddddddd " + data);
         res.json({id: data});
     });
 });
 router.put("/index",(req,res)=>{
     burger.modify("devoured",true,req.body.id,(data)=>{
-        console.log(req.body.id);
-        // res.json({});
     });
 });
 
